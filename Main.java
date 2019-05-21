d1114a1a4ee8a113ce53c838f54107da7bd40b3a
Dandridge Cole <dandridgecole@yahoo.com>
2019-05-21 16:39:26 -0400

    My Alice And Bob

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+//import java.lang.System;
 public class Main {
 
     public static void main(String[] args ){
-
+        Scanner myInput = new Scanner(System.in);
+        System.out.print("Please enter your name:");
+        String name = myInput.nextLine();
+        if (name.equals("Bob")|| name.equals("Alice")){
+            System.out.print("Howdy, "+name+"!");
+        } else{
+            System.out.print("Get outta my house!");
+        }
     }
 }
